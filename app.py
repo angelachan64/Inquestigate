b72603b8755d54ab184f8b6d124159e2eece6585
backfrip <backfrip@users.noreply.github.com>
2015-11-13 18:15:00 +0000

    Took out that dumb comment. Also, I don't think pylint likes 'app' or '__name__==__main__'. Should that be changed? Meh.

--- app.py
+++ app.py
@@ -19,16 +19,3 @@ if __name__=="__main__":
     app.debug = True
     app.secret_key = "p0NZLhPzCbjSJxxo"
     app.run(host='0.0.0.0', port=8000)
-
-
-"""
-    data = nytimes.top_stories('technology')
-    output = "<head><style>div{background-color:#cccccc;padding:1em;margin:\
-1em;font-family:sans-serif;width:40%;display:inline-block;vertical-align:top;\
-}</style></head><body>"
-    for article in range(len(data)):
-        output += u"<div id=\"{0}\"><h2 style=\"float:right;color:#808080\">\
-<i>{0}</i></h2>{1}</div>".format(
-            article, nytimes.format_article(data[article]))
-    return output + "</body>"
-"""
