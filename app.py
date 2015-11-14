07c1af3eebe1460121028da63b2e126133180d5b
backfrip <backfrip@users.noreply.github.com>
2015-11-14 20:28:39 +0000

    Just a comment addition, but I wanna test if authors still work without a global user configuration.

--- app.py
+++ app.py
@@ -20,4 +20,6 @@ if __name__=="__main__":
     app.secret_key = "p0NZLhPzCbjSJxxo"
     app.run(host='0.0.0.0',
         port=(8080 if os.path.isfile('cloudy') else 8000)
+        # (Account for the fact that Cloud9 doesn't do port 8000
+        #  with an arbitrary file check. Don't worry, Git ignores it.)
     )
